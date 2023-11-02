e48a96f006f3fde8819b64ac744681415bf556d3
Yuliia <djudit@ukr.net>
2023-11-02 03:52:16 +0200

    task 1 finished

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,14 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const listIngredients = document.querySelector("#ingredients");
+
+const firstElem = document.createElement("li");
+firstElem.textContent = "Potatoes";
+listIngredients.append(firstElem);
