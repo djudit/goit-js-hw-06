ab239a8506affb9c7a5b3a77b3f7272f83b71700
Yuliia <djudit@ukr.net>
2023-11-04 00:42:03 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,7 +1,7 @@
 const btnDecrement = document.querySelector('button[data-action="decrement"]');
 const btnIncrement = document.querySelector('button[data-action="increment"]');
 const spanSum = document.querySelector("#value");
-console.dir(spanSum);
+// console.dir(spanSum);
 
 let counterValue = 0;
 
