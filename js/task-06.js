2536e1973591d6c6db039bcf54045b8733ff6a30
Yuliia <djudit@ukr.net>
2023-11-04 19:50:22 +0200

    Task 6 finished

--- js/task-06.js
+++ js/task-06.js
@@ -1,12 +1,24 @@
 const inputSymbols = document.querySelector("#validation-input");
+console.dir(inputSymbols);
 
-const handleInput = (event) => {
-  if (inputSymbols.textContent.length === 6) {
-    inputSymbols.classList.add("valid");
-  } else {
+inputSymbols.addEventListener("input", handleInput);
+
+function handleInput(event) {
+  // event.preventDefault();
+  if (
+    event.currentTarget.value.length !==
+    Number.parseInt(inputSymbols.dataset.length)
+  ) {
     inputSymbols.classList.add("invalid");
+  } else {
+    inputSymbols.classList.remove("invalid");
+    inputSymbols.classList.add("valid");
   }
-  //   inputSymbols.textContent = event.currentTarget.value;
-};
 
-inputSymbols.addEventListener("blur", handleInput);
+  // console.log(event.currentTarget.value.length);
+  console.log(inputSymbols.dataset.length);
+}
+
+// inputSymbols.addEventListener("input", handleInput);
+// inputSymbols.addEventListener("focus", handleInput);
+// inputSymbols.addEventListener("blur", handleInput);
