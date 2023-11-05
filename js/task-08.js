9be031e12f780f84ae78e7c654c5bf3c60b3d1db
Yuliia <djudit@ukr.net>
2023-11-06 01:26:59 +0200

    Task 8 finished

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const registerForm = document.querySelector(".login-form");
 
+registerForm.addEventListener("submit", handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const form = event.target;
+  const email = form.elements.email;
+  const password = form.elements.password;
+
+  const objUser = {};
+
+  if (email.value === "" || password.value === "") {
+    return alert("Please fill in all the fields!");
+  }
+
+  objUser.email = email.value;
+  objUser.password = password.value;
+
+  console.log(objUser);
+
+  alert(`Email: ${email.value}, Password: ${password.value}`);
+  form.reset();
+}
