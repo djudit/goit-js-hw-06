18ec608f3e2e778add4611e40441ddfb88601468
Yuliia <djudit@ukr.net>
2023-11-06 16:35:16 +0200

    add trim() for task 5

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,7 @@ const inputName = document.querySelector("#name-input");
 const spanName = document.querySelector("#name-output");
 
 const handleInput = (event) => {
+  inputName.value = inputName.value.trim();
   if (inputName.value === "") {
     spanName.textContent = "Anonymous";
   } else {
