fd53383e23a91d0179ec8bccc61cefb4877d568c
Yuliia <djudit@ukr.net>
2023-11-06 14:26:28 +0200

    Task 9 finished

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,19 @@
+const buttonBg = document.querySelector(".change-color");
+const spanColor = document.querySelector(".color");
+const bodyBG = document.querySelector("body");
+
+buttonBg.addEventListener("click", handleClick);
+
+function handleClick(event) {
+  if (event.target.nodeName !== "BUTTON") {
+    return;
+  }
+
+  bodyBG.style.backgroundColor = getRandomHexColor();
+
+  spanColor.textContent = bodyBG.style.backgroundColor;
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
