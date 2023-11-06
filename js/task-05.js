b417ed1f3773de5afef3c2dd479e92c6797d075f
Yuliia <djudit@ukr.net>
2023-11-06 15:34:34 +0200

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const inputName = document.querySelector("#name-input");
 const spanName = document.querySelector("#name-output");
 
 const handleInput = (event) => {
-  if (inputName.textContent === null) {
+  if (inputName.value === "") {
     spanName.textContent = "Anonymous";
   } else {
     spanName.textContent = event.currentTarget.value;
